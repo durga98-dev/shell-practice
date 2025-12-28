9e6c0c4415aacdf87be70d26a1e055216f58bf01
Durga <durgadevi0707@gmail.com>
2025-12-28 23:16:09 +0530

    even odd script

--- 04-installation.sh
+++ 04-installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Run the script with root privilege"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: Mysql installation is Failed"
+    exit 1
 else
     echo "MySQL installation is success"
 fi
